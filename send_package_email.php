c7b8b6b699411cfe3e3d203f5cba85d6bb251c8e
alihassan <alihassan.workmail@gmail.com>
2022-10-17 12:07:15 +0400

    Add email integration

--- send_package_email.php
+++ send_package_email.php
@@ -12,124 +12,146 @@ if (isset($_POST["name"])) {
 	$senderPackage = ($_POST['selectedPackage']);
 	$senderSubPackage = ($_POST['selectedPackageSub']);
 
+	$packages =array (
+		0 => 
+		array (
+		  '$id' => 1,
+		  '$label' => 'Basic',
+		  '$cars' => 5,
+		  '$plans' => 
+		  array (
+			0 => 
+			array (
+			  '$id' => 1,
+			  '$months' => 3,
+			  '$price' => '27,500',
+			),
+			1 => 
+			array (
+			  '$id' => 2,
+			  '$months' => 6,
+			  '$price' => '52,000',
+			),
+			2 => 
+			array (
+			  '$id' => 3,
+			  '$months' => 9,
+			  '$price' => '75,750',
+			),
+			3 => 
+			array (
+			  '$id' => 4,
+			  '$months' => 12,
+			  '$price' => '98,000',
+			),
+		  ),
+		),
+		1 => 
+		array (
+		  '$id' => 2,
+		  '$label' => 'Basic Plus',
+		  '$cars' => 15,
+		  '$plans' => 
+		  array (
+			0 => 
+			array (
+			  '$id' => 1,
+			  '$months' => 3,
+			  '$price' => '81,000',
+			),
+			1 => 
+			array (
+			  '$id' => 2,
+			  '$months' => 6,
+			  '$price' => '153,000',
+			),
+			2 => 
+			array (
+			  '$id' => 3,
+			  '$months' => 9,
+			  '$price' => '223,425',
+			),
+			3 => 
+			array (
+			  '$id' => 4,
+			  '$months' => 12,
+			  '$price' => '288,000',
+			),
+		  ),
+		),
+		2 => 
+		array (
+		  '$id' => 3,
+		  '$label' => 'Premium',
+		  '$cars' => 30,
+		  '$plans' => 
+		  array (
+			0 => 
+			array (
+			  '$id' => 1,
+			  '$months' => 3,
+			  '$price' => '157,500',
+			),
+			1 => 
+			array (
+			  '$id' => 2,
+			  '$months' => 6,
+			  '$price' => '297,000',
+			),
+			2 => 
+			array (
+			  '$id' => 3,
+			  '$months' => 9,
+			  '$price' => '433,350',
+			),
+			3 => 
+			array (
+			  '$id' => 4,
+			  '$months' => 12,
+			  '$price' => '558,000',
+			),
+		  ),
+		),
+		3 => 
+		array (
+		  '$id' => 4,
+		  '$cars' => 50,
+		  '$label' => 'Pro',
+		  '$plans' => 
+		  array (
+			0 => 
+			array (
+			  '$id' => 1,
+			  '$months' => 3,
+			  '$price' => '255,000',
+			),
+			1 => 
+			array (
+			  '$id' => 2,
+			  '$months' => 6,
+			  '$price' => '480,000',
+			),
+			2 => 
+			array (
+			  '$id' => 3,
+			  '$months' => 9,
+			  '$price' => '699,750',
+			),
+			3 => 
+			array (
+			  '$id' => 4,
+			  '$months' => 12,
+			  '$price' => '900,000',
+			),
+		  ),
+		),
+	);
 
-
-
-	$packages =json_decode(json_decode('[
-		{
-		  id: 1,
-		  label: "Basic",
-		  cars: 5,
-		  plans: [
-			{
-			  id: 1,
-			  months: 3,
-			  price: "27,500",
-			},
-			{
-			  id: 2,
-			  months: 6,
-			  price: "52,000",
-			},
-			{
-			  id: 3,
-			  months: 9,
-			  price: "75,750",
-			},
-			{
-			  id: 4,
-			  months: 12,
-			  price: "98,000",
-			},
-		  ],
-		},
-		{
-		  id: 2,
-		  label: "Basic Plus",
-		  cars: 15,
-		  plans: [
-			{
-			  id: 1,
-			  months: 3,
-			  price: "81,000",
-			},
-			{
-			  id: 2,
-			  months: 6,
-			  price: "153,000",
-			},
-			{
-			  id: 3,
-			  months: 9,
-			  price: "223,425",
-			},
-			{
-			  id: 4,
-			  months: 12,
-			  price: "288,000",
-			},
-		  ],
-		},
-		{
-		  id: 3,
-		  label: "Premium",
-		  cars: 30,
-		  plans: [
-			{
-			  id: 1,
-			  months: 3,
-			  price: "157,500",
-			},
-			{
-			  id: 2,
-			  months: 6,
-			  price: "297,000",
-			},
-			{
-			  id: 3,
-			  months: 9,
-			  price: "433,350",
-			},
-			{
-			  id: 4,
-			  months: 12,
-			  price: "558,000",
-			},
-		  ],
-		},
-		{
-		  id: 4,
-		  cars: 50,
-		  label: "Pro",
-		  plans: [
-			{
-			  id: 1,
-			  months: 3,
-			  price: "255,000",
-			},
-			{
-			  id: 2,
-			  months: 6,
-			  price: "480,000",
-			},
-			{
-			  id: 3,
-			  months: 9,
-			  price: "699,750",
-			},
-			{
-			  id: 4,
-			  months: 12,
-			  price: "900,000",
-			},
-		  ],
-		}
-	  ]'));
 	  
 
 	
-	$currentPackage = $packages[intval($senderPackage)-1]
-	$currentPackageSub = $currentPackage.$plans[intval($senderSubPackage)-1]
+	$currentPackage = $packages[intval($senderPackage)-1];
+	$currentPackageSub = $currentPackage['$plans'][intval($senderSubPackage)-1];
 	  // $senderMessage = isset($_POST['message']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['message']) : "";
 	if ($userName == 'Henrynus') {
 		header("Location: contactsuccess.html");
@@ -151,12 +173,14 @@ if (isset($_POST["name"])) {
 			Email: " . $senderEmail . "<br>
 			Company Name: " . $senderCompanyName . "<br>
 			Phone Number: " . $senderPhoneNumber . "<br>
-			Number of Cars: " . $currentPackage.$cars . "<br>
- 			Advertisement Duration: " .  $currentPackageSub.$months . "<br>
-			Advertisement Price: " .  $currentPackageSub.$price . "<br>"
+			Number of Cars: " . $currentPackage['$cars'] . "<br>
+ 			Advertisement Duration: " .  $currentPackageSub['$months'] . "<br>
+			Advertisement Price: " .  $currentPackageSub['$price'] . "<br>"
 			. "";
 
-		//Email Send Function
+	
+
+	
 		$send_email = mail($to, $subject, $message, $headers);
 
 		header("Location: contactsuccess.html");
