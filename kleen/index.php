c96a47f025cf50eae69aef445f05849d27bd801c
faisal <faisal@mvp-apps.ae>
2022-11-01 12:15:29 +0400

    added client on title

--- kleen/index.php
+++ kleen/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Bseen | 3DView</title>
+  <title><?php echo $clientName ?> Bseen | 3DView</title>
 
   <meta charset="UTF-8" />
   <script type="text/javascript">
